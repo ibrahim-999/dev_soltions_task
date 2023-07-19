304e4ebfc85ad8926b3d39f6c3a43d033c3d4601
“ibrahim-999” <ibrahimkhalaf99@gmail.com>
2023-07-19 21:30:42 +0300

    [Add] employee attributes

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone')->unique()->nullable();
+            $table->string('job_title')->nullable();
+            $table->integer('salary')->nullable();
+            $table->string('department')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
